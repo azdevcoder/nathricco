01f11aa5bb932a2ad91415aaa940fe2fd29da6d2
AzDevcoder <adm.azdevcoder@gmail.com>
2026-01-30 11:34:17 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'nath-agenda-v1';
+const assets = [
+  './',
+  './index.html',
+  './contrato.html',
+  './pacientes.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
