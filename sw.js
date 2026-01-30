0c320142184191e14d70259517a19bb4ad2dbe45
AzDevcoder <adm.azdevcoder@gmail.com>
2026-01-30 17:19:46 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'nath-ricco-v1';
+const ASSETS = [
+  '/nathricco/',
+  '/nathricco/index.html',
+  '/nathricco/pacientes.html',
+  '/nathricco/contrato.html',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
