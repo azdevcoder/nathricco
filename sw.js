4f5ae3d5ef57880ce992bf78591b3c5a47078784
AzDevcoder <adm.azdevcoder@gmail.com>
2026-01-30 22:20:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nath-ricco-v6'; // Incrementei a versão
+const CACHE_NAME = 'nath-ricco-v10';
 const ASSETS = [
   './',
   './index.html',
@@ -9,36 +9,23 @@ const ASSETS = [
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css'
 ];
 
-// Instalação e Cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('PWA: Arquivos em cache');
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Ativação e Limpeza de lixo
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
+    ))
   );
-  return self.clients.claim();
 });
 
-// Estratégia: Cache primeiro, depois rede
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
