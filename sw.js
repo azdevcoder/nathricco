01e2211736299127cb75720b2c6cae0a30140f76
AzDevcoder <adm.azdevcoder@gmail.com>
2026-01-31 11:18:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,10 @@
-const cacheName = 'nath-agenda-v31';
-const assets = [
-  './',
-  './index.html',
-  './contrato.html',
-  './pacientes.html',
-  './lista_contratos.html',
-  './enviar_contrato.html',
-  './manifest.json',
-  './calendar.png'
-];
-
+const cacheName = 'nath-agenda-v35';
+const assets = ['./index.html', './contrato.html', './pacientes.html', './lista_contratos.html', './manifest.json', './calendar.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets)));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== cacheName).map(k => caches.delete(k)))));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
